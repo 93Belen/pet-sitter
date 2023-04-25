fc4dc46531e4d12d320b934c8a57e1156c3a39cc
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-04-25 06:24:50 -0500

    pricing page functionality done

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,7 +1,13 @@
 "use client"
 
-import { useState } from "react"
+import React, { useEffect, useState } from "react"
 import {IoClose} from 'react-icons/io5'
+
+
+type dayInfo = {
+  description: string,
+  price: number
+}
 
 
 export default function Pricing() {
@@ -9,6 +15,12 @@ const [description, setDescription] = useState('')
 const [price, setPrice] = useState(0)
 const [id, setId] = useState<Number | undefined>()
 const [show, setShow] = useState('hidden')
+const [halfDayInfo, setHalfDayInfo] = useState<dayInfo>()
+const [fullDayInfo, setFullDayInfo] = useState<dayInfo>()
+const [dayAndNightInfo, setdayAndNightInfo] = useState<dayInfo>()
+
+
+
 
 
   const editPricing = async(id: number) => {
@@ -23,22 +35,37 @@ const [show, setShow] = useState('hidden')
     })
   }
 
-  const handleDescriptionChange = (event) => {
+  const handleDescriptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setDescription(event.target.value)
   }
-  const handlePriceChange = (event) => {
+  const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPrice(Number(event.target.value))
   }
   const handleIdChange = (newId: number) => {
     setShow('')
     setId(newId)
   }
-  const showForm = () => {
-    setShow('')
-  }
   const hideForm = () => {
     setShow('hidden')
   }
+
+const getPricing = async() => {
+  const res = await fetch('api/getPricing')
+  const jsonRes = await res.json();
+  return jsonRes;
+}
+
+
+const updatePricing = () => {
+  getPricing().then(res => {
+    setHalfDayInfo(res[0])
+    setFullDayInfo(res[1])
+    setdayAndNightInfo(res[2])
+  })
+}
+
+updatePricing()
+
 
 
     return (
@@ -47,36 +74,27 @@ const [show, setShow] = useState('hidden')
         <IoClose onClick={hideForm} />
         <textarea maxLength={200} onChange={handleDescriptionChange} className='rounded-lg p-7' cols={10} rows={12} name="description" id="description" />
         <input onChange={handlePriceChange} className='rounded-lg px-6 p-5' type="number" min={3} name="price" id="price" />
-        <button onClick={() => {editPricing(id as number)}} className='bg-mytheme py-3 px-6 rounded-full w-fit'>Update!</button>
+        <button onClick={() => {
+          editPricing(id as number).then(() => updatePricing())
+          setShow('hidden')
+          }} className='bg-mytheme py-3 px-6 rounded-full w-fit'>Update!</button>
       </div>
        <div className='p-6 bg-mygray rounded-lg flex flex-col gap-5'>
          <h2 className='font-bold text-xl'>Half Day</h2>
-         <p>Lorem ipsum dolor sit amet consectetur 
-           adipisicing elit. Culpa similique quia 
-           ferendis cum provident beatae unde dol
-           dolore! Quas explicabo magni ipsa accus
-           olorem ullam, consequuntur officiis. Sequi!</p>
-          <p className='font-extrabold text-3xl m-auto'>$9.99 /h</p>
+         <p>{halfDayInfo?.description}</p>
+          <p className='font-extrabold text-3xl m-auto'>${halfDayInfo?.price}</p>
           <button onClick={() => {handleIdChange(1)}} className='bg-mytheme py-3 px-6 rounded-full w-fit m-auto'>Edit</button>
        </div>
        <div className='p-6 bg-mygray rounded-lg flex flex-col gap-5'>
          <h2 className='font-bold text-xl'>All Day</h2>
-         <p>Lorem ipsum dolor sit amet consectetur 
-           adipisicing elit. Culpa similique quia 
-           ferendis cum provident beatae unde dol
-           dolore! Quas explicabo magni ipsa accus
-           olorem ullam, consequuntur officiis. Sequi!</p>
-          <p className='font-extrabold text-3xl m-auto'>$9.99 /h</p>
+         <p>{fullDayInfo?.description}</p>
+          <p className='font-extrabold text-3xl m-auto'>${fullDayInfo?.price}</p>
           <button onClick={() => {handleIdChange(2)}} className='bg-mytheme py-3 px-6 rounded-full w-fit m-auto'>Edit</button>
        </div>
        <div className='p-6 bg-mygray rounded-lg flex flex-col gap-5'>
          <h2 className='font-bold text-xl'>Day and Overnight</h2>
-         <p>Lorem ipsum dolor sit amet consectetur 
-           adipisicing elit. Culpa similique quia 
-           ferendis cum provident beatae unde dol
-           dolore! Quas explicabo magni ipsa accus
-           olorem ullam, consequuntur officiis. Sequi!</p>
-          <p className='font-extrabold text-3xl m-auto'>$9.99 /h</p>
+         <p>{dayAndNightInfo?.description}</p>
+          <p className='font-extrabold text-3xl m-auto'>${dayAndNightInfo?.price}</p>
           <button onClick={() => {handleIdChange(3)}} className='bg-mytheme py-3 px-6 rounded-full w-fit m-auto'>Edit</button>
        </div>
       </main>
