38a6275156daedde57c342d17d6d260fc676eeed
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-04-25 05:43:50 -0500

    pricing page working

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,13 +1,14 @@
 "use client"
 
 import { useState } from "react"
+import {IoClose} from 'react-icons/io5'
 
 
 export default function Pricing() {
 const [description, setDescription] = useState('')
 const [price, setPrice] = useState(0)
 const [id, setId] = useState<Number | undefined>()
-const [show, setShow] = useState(false)
+const [show, setShow] = useState('hidden')
 
 
   const editPricing = async(id: number) => {
@@ -29,16 +30,24 @@ const [show, setShow] = useState(false)
     setPrice(Number(event.target.value))
   }
   const handleIdChange = (newId: number) => {
+    setShow('')
     setId(newId)
+  }
+  const showForm = () => {
+    setShow('')
+  }
+  const hideForm = () => {
+    setShow('hidden')
   }
 
 
     return (
       <main className='pt-10 flex gap-10'>
-      <div className='absolute bg-mygray w-3/6 h-screen top-0 right-0 flex flex-col justify-around p-10'>
+      <div className={`absolute bg-mygray w-3/6 h-screen top-0 right-0 flex flex-col justify-around p-10 ${show}`}>
+        <IoClose onClick={hideForm} />
         <textarea maxLength={200} onChange={handleDescriptionChange} className='rounded-lg p-7' cols={10} rows={12} name="description" id="description" />
         <input onChange={handlePriceChange} className='rounded-lg px-6 p-5' type="number" min={3} name="price" id="price" />
-        <button onClick={() => {editPricing(id)}} className='bg-mytheme py-3 px-6 rounded-full w-fit'>Update!</button>
+        <button onClick={() => {editPricing(id as number)}} className='bg-mytheme py-3 px-6 rounded-full w-fit'>Update!</button>
       </div>
        <div className='p-6 bg-mygray rounded-lg flex flex-col gap-5'>
          <h2 className='font-bold text-xl'>Half Day</h2>
