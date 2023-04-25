9c8afdb916023a82378a58bfbf9bf3a68b740853
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-04-25 05:29:13 -0500

    updates id of pricing to change

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -6,6 +6,7 @@ import { useState } from "react"
 export default function Pricing() {
 const [description, setDescription] = useState('')
 const [price, setPrice] = useState(0)
+const [id, setId] = useState<Number | undefined>()
 const [show, setShow] = useState(false)
 
 
@@ -27,6 +28,9 @@ const [show, setShow] = useState(false)
   const handlePriceChange = (event) => {
     setPrice(Number(event.target.value))
   }
+  const handleIdChange = (newId: number) => {
+    setId(newId)
+  }
 
 
     return (
@@ -34,7 +38,7 @@ const [show, setShow] = useState(false)
       <div className='absolute bg-mygray w-3/6 h-screen top-0 right-0 flex flex-col justify-around p-10'>
         <textarea maxLength={200} onChange={handleDescriptionChange} className='rounded-lg p-7' cols={10} rows={12} name="description" id="description" />
         <input onChange={handlePriceChange} className='rounded-lg px-6 p-5' type="number" min={3} name="price" id="price" />
-        <button onClick={() => {editPricing(1)}} className='bg-mytheme py-3 px-6 rounded-full w-fit'>Update!</button>
+        <button onClick={() => {editPricing(id)}} className='bg-mytheme py-3 px-6 rounded-full w-fit'>Update!</button>
       </div>
        <div className='p-6 bg-mygray rounded-lg flex flex-col gap-5'>
          <h2 className='font-bold text-xl'>Half Day</h2>
@@ -44,7 +48,7 @@ const [show, setShow] = useState(false)
            dolore! Quas explicabo magni ipsa accus
            olorem ullam, consequuntur officiis. Sequi!</p>
           <p className='font-extrabold text-3xl m-auto'>$9.99 /h</p>
-          <button className='bg-mytheme py-3 px-6 rounded-full w-fit m-auto'>Edit</button>
+          <button onClick={() => {handleIdChange(1)}} className='bg-mytheme py-3 px-6 rounded-full w-fit m-auto'>Edit</button>
        </div>
        <div className='p-6 bg-mygray rounded-lg flex flex-col gap-5'>
          <h2 className='font-bold text-xl'>All Day</h2>
@@ -54,7 +58,7 @@ const [show, setShow] = useState(false)
            dolore! Quas explicabo magni ipsa accus
            olorem ullam, consequuntur officiis. Sequi!</p>
           <p className='font-extrabold text-3xl m-auto'>$9.99 /h</p>
-          <button className='bg-mytheme py-3 px-6 rounded-full w-fit m-auto'>Edit</button>
+          <button onClick={() => {handleIdChange(2)}} className='bg-mytheme py-3 px-6 rounded-full w-fit m-auto'>Edit</button>
        </div>
        <div className='p-6 bg-mygray rounded-lg flex flex-col gap-5'>
          <h2 className='font-bold text-xl'>Day and Overnight</h2>
@@ -64,7 +68,7 @@ const [show, setShow] = useState(false)
            dolore! Quas explicabo magni ipsa accus
            olorem ullam, consequuntur officiis. Sequi!</p>
           <p className='font-extrabold text-3xl m-auto'>$9.99 /h</p>
-          <button className='bg-mytheme py-3 px-6 rounded-full w-fit m-auto'>Edit</button>
+          <button onClick={() => {handleIdChange(3)}} className='bg-mytheme py-3 px-6 rounded-full w-fit m-auto'>Edit</button>
        </div>
       </main>
     )
